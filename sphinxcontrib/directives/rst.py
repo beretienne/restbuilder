19744627330869f7b981d504fa018d6283fdc307
Bernard Etiennot <b.etiennot@gmail.com>
2023-05-26 10:40:39 +0200

    [FIX] keep normal include behaviour when builder is not 'rst'

--- sphinxcontrib/directives/rst.py
+++ sphinxcontrib/directives/rst.py
@@ -5,12 +5,15 @@ class Include(Include):
     """ Override the default Sphinx Include class by creating a new include node that insert raw content """
 
     def run(self):
-        current_line = self.lineno - self.state_machine.input_offset - 1
-        if ('rst_prolog' in self.state_machine.input_lines.items[current_line][0]):
-            return []       # Skip includes from rst_prolog
-        attributes = {'format': 'text'}
-        node = include('', self.block_text, classes=self.options.get('class', []), **attributes)
-        (node.source,
-         node.line) = self.state_machine.get_source_and_line(self.lineno)
-        return [node]
+        if (self.env.app.builder.name == 'rst'):
+            current_line = self.lineno - self.state_machine.input_offset - 1
+            if ('rst_prolog' in self.state_machine.input_lines.items[current_line][0]):
+                return []       # Skip includes from rst_prolog
+            attributes = {'format': 'text'}
+            node = include('', self.block_text, classes=self.options.get('class', []), **attributes)
+            (node.source,
+             node.line) = self.state_machine.get_source_and_line(self.lineno)
+            return [node]
+        else:
+            return super().run()
 
